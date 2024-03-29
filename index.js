442a7ee64a1539e6eb9718fbb659295290335523
frastoneme <oigoeme@gmail.com>
2024-03-29 09:31:31 +0300

    adds a filter function

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(driver, search) {
+    return driver.filter(
+      (possibleMatch) => possibleMatch.toLowerCase() === search.toLowerCase()
+    );
+  }
+  
+  function fuzzyMatch(driver, testString) {
+    return driver.filter(
+      (possibleMatch) =>
+        possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    );
+  }
+  
+  function matchName(driver, soughtName) {
+    return driver.filter((record) => record.name === soughtName);
+  }
